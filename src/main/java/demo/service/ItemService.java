c3307aa3f0d168e0b7d026da0162310dfa018472
rgolder1 <rgolder1@yahoo.com>
2024-02-22 16:41:04 +0000

    Update.

--- src/main/java/demo/service/ItemService.java
+++ src/main/java/demo/service/ItemService.java
@@ -12,7 +12,9 @@ import demo.rest.api.UpdateItemRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 @Slf4j
 public class ItemService {
